d5d255dea80cb138d2056eaace7dac0126d1dfcf
QSL <kbeechly@icloud.com>
2022-06-21 11:09:56 -0700

    Add empty html and css, add basic operator functions

---
+++ calc.js
@@ -0,0 +1,24 @@
+var x
+var y
+var z
+
+function add() {
+    z = x + y
+}
+
+function sub() {
+    z = x - y
+}
+
+function div() {
+    z = x / y
+}
+
+function mult() {
+    z = x * y
+}
+
+function eq() {
+    console.log(z)
+    x = z
+}
